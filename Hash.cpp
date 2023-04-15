dd33f1108fec84611dcaa5022caf780024f6b4bb
albrown11 <129125977+albrown11@users.noreply.github.com>
2023-04-15 17:16:31 -0400

    Add files via upload

---
+++ Hash.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	srand(time(NULL));
+	int x[120],j[120],i, k, r, c;
+	string s;
+	cout << "Enter message to convert: ";
+	getline(cin, s);
+	k = rand() % 100 + 1;
+	if (k % 2 == 0 || k % 3 == 0 || k % 5 == 0 || k % 7 == 0)
+	{
+		k = rand() % 100 + 1;
+	}
+	for (i=0;i < s.length();i++)
+	{
+		(int)s[i];
+		x[i] = (int)s[i];
+	}
+	for (c=0;c < s.length();c++)
+	{
+		r = rand() % 12 + 1;
+		j[c] = x[c] + pow(k, r);
+		j[c] = j[c] % 128;
+		cout << j[c];
+	}
+	system("pause");
+	return 0;
+}
